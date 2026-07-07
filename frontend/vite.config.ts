9be2496c7695372d3c3e0a311d8d30bd75a10d43
bimo3058 <bimo3058@gmail.com>
2026-07-07 20:48:42 +0700

    Initial commit: setup Vue+TS PWA frontend & Supabase backend

---
+++ frontend/vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "TanjungMas",
+        short_name: "TJMas",
+        description: "Deskripsi aplikasi",
+        theme_color: "#0B266E",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
